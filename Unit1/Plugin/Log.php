9f3fbda37bc98ea27ca3af823c192b5184164173
Pedro Fernandez <pedferna@adobe.com>
2022-07-26 12:31:17 +0200

    implement log plugin

---
+++ Unit1/Plugin/Log.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Unit1\CustomerFormDump\Plugin;
+
+class Log
+{
+    /**
+     * \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * afterGetData
+     *
+     * @param mixed $subject
+     * @param mixed $result
+     *
+     * @return void
+     */
+    public function afterGetData(\Magento\Customer\Model\Customer\DataProviderWithDefaultAddresses $subject, $result)
+    {
+        foreach($result as $customer){
+            $this->logger->debug('data: ', $customer);
+        }
+        return $result;
+    }
+}
+
